3e98700dbb81b9d727c288323048b35ba00f83e3
Linzy-Liu <die.sturmz@gmail.com>
2023-08-30 01:09:15 +0800

    Update model.py

--- MECPE/model.py
+++ MECPE/model.py
@@ -17,6 +17,53 @@ def get_mask(length, max_len, out_shape):
     return ret
 
 
+def concat_feature(x, x_audio, x_video):
+    if x_audio is not None:
+        x = torch.cat([x, x_audio], dim=-1)
+    if x_video is not None:
+        x = torch.cat([x, x_video], dim=-1)
+    return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __int__(self, n_hidden, n_head=8, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        self.n_hidden = n_hidden
+        self.n_head = n_head
+        self.d_k = n_hidden // n_head
+
+
+class RealTimeTransformer(nn.Module):
+    def __init__(self, n_hidden, n_head=1):
+        super(RealTimeTransformer, self).__init__()
+        # Multi-head Attention mechanism
+
+        # Point-wise Feedforward network
+        self.fc1 = nn.Linear(n_hidden, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        # Layer normalization
+        self.norm1 = nn.LayerNorm(n_hidden)
+        self.norm2 = nn.LayerNorm(n_hidden)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # x shape: [batch_size, max_len, n_hidden]
+        # Multi-head Attention requires input as [max_len, batch_size, n_hidden]
+        x_transposed = x.transpose(0, 1)
+
+        # Multi-head Attention
+
+        x = self.norm1(attn_output + x_transposed)
+        # Point-wise Feedforward
+        output = self.relu(self.fc1(x))
+        output = self.fc2(output)
+        output = self.norm2(output + x)
+
+        # Convert shape back to [batch_size, max_len, n_hidden]
+        output = output.transpose(0, 1)
+        return output
+
+
 class MECPEStep1(nn.Module):
     def __init__(self,
                  hidden_dim,
@@ -64,14 +111,16 @@ class MECPEStep1(nn.Module):
         config.output_hidden_states = True
         config.hidden_dropout_prob = dropout_bert_hidden
         config.attention_probs_dropout_prob = dropout_bert_attention
+        self.relu = nn.ReLU()
+        self.norm = nn.LayerNorm(hidden_dim)
         self.BERT = BertModel.from_pretrained('bert-base-uncased', config=config)
         # linear settings
         self.linear_audio_emotion = nn.Linear(6373, hidden_dim)
         self.linear_audio_cause = nn.Linear(6373, hidden_dim)
         self.linear_video_emotion = nn.Linear(4096, hidden_dim)
         self.linear_video_cause = nn.Linear(4096, hidden_dim)
-        self.linear_fusion_emotion = nn.Linear(hidden_dim * 2 + 768, hidden_dim)
-        self.linear_fusion_cause = nn.Linear(hidden_dim * 2 + 768, hidden_dim)
+        self.linear_fusion_emotion = nn.Linear(hidden_dim * np.sum([self.use_audio, self.use_video]) + 768, hidden_dim)
+        self.linear_fusion_cause = nn.Linear(hidden_dim * np.sum([self.use_audio, self.use_video]) + 768, hidden_dim)
 
     def forward(self, x_a_v, bert_input, diag_len):
         """
@@ -81,15 +130,44 @@ class MECPEStep1(nn.Module):
         :param diag_len: The length of each dialogue.
         :return: The output of the first step prediction.
         """
-        x_audio, x_video = x_a_v
+        if x_a_v is None:
+            x_audio, x_video = None, None
+        else:
+            x_audio, x_video = x_a_v
         x_bert_sent, x_bert_sent_mask = bert_input
-        # x_audio shape:[batch_size, max_utt_num, 6373]
-        # x_video shape:[batch_size, max_utt_num, 4096]
+        # x_audio shape:[batch_size, max_utt_num]
+        # x_video shape:[batch_size, max_utt_num]
         # x_bert_sent shape:[batch_size, max_utt_num, max_sent_len]
         # x_bert_sent_mask shape:[batch_size, max_utt_num, max_sent_len]
         # diag_len shape:[batch_size]
 
-        x_audio = F.embedding(x_audio, self.audio_embeddings)
-        x_video = F.embedding(x_video, self.video_embeddings)
-        x_audio = self.dropout_audio(x_audio)
-        x_video = self.dropout_video(x_video)
+        feature_mask = get_mask(diag_len, self.max_shape[0], [-1, self.max_shape[0], 1])
+        x_bert_sen = x_bert_sent.view(-1, self.max_shape[1])
+        x_mask_bert_sen = x_bert_sent_mask.view(-1, self.max_shape[1])
+        # 4. 进行前向传播以获取pooled_output
+        outputs = self.BERT(x_bert_sen, attention_mask=x_mask_bert_sen)
+        s_bert = outputs[1]
+        s_bert = s_bert.view(-1, self.max_shape[0], s_bert.shape[-1])
+        s_bert = s_bert * feature_mask
+
+        if x_audio is not None:
+            x_audio = F.embedding(x_audio, self.audio_embeddings)
+            x_audio = self.dropout_audio(x_audio)
+            x_audio_emotion = self.relu(self.norm(self.linear_audio_emotion(x_audio)))
+            x_audio_cause = self.relu(self.norm(self.linear_audio_cause(x_audio)))
+
+        if x_video is not None:
+            x_video = F.embedding(x_video, self.video_embeddings)
+            x_video = self.dropout_video(x_video)
+            x_video_emotion = self.relu(self.norm(self.linear_video_emotion(x_video)))
+            x_video_cause = self.relu(self.norm(self.linear_video_cause(x_video)))
+
+        x_emotion = concat_feature(s_bert, x_audio_emotion if self.use_audio else None,
+                                   x_video_emotion if self.use_video else None)
+        x_emotion = x_emotion * feature_mask
+        x_emotion = self.linear_fusion_emotion(x_emotion)
+
+        x_cause = concat_feature(s_bert, x_audio_cause if self.use_audio else None,
+                                 x_video_cause if self.use_video else None)
+        x_cause = x_cause * feature_mask
+        x_cause = self.linear_fusion_cause(x_cause)
